54cd672b0a7a5ed4c206e8a808c200163f41dd8d
jbossgotskillz <160427209+jbossgotskillz@users.noreply.github.com>
2024-07-30 00:52:28 -0500

    Update admin.py

--- stock/admin.py
+++ stock/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 from .models import Stock, Detail
 
- 
-
 class StockAdmin(admin.ModelAdmin):
 
    list_display = ('company', 'symbol', 'quantity', 'value', 'balance')
@@ -15,13 +13,3 @@ class DetailAdmin(admin.ModelAdmin):
    list_display = ('company', 'description', 'est_date')
 
 admin.site.register(Detail, DetailAdmin)
-
-
-"""
-createsuperuser inputs:
-
-username = jordanboss
-email = jawoo25@hotmail.com
-password = Therealjboss#1!
-confirm password = Therealjboss#1!
-"""
