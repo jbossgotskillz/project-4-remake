be9535b21bae8acc9c35ab2779d0329ea8c1a1d7
jbossgotskillz <160427209+jbossgotskillz@users.noreply.github.com>
2024-07-30 01:02:46 -0500

    Update views.py

--- stock/views.py
+++ stock/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from django.http import Http404
+# from django.http import Http404
 from .models import Stock, Detail
-# from .forms import StockForm, DetailForm
-
-# def index(request):
-#	return render(request,'template.html')
 
 def index(request):
-	return render(request, 'index.html')	# the original base template
+	return render(request, 'index.html')
 
 def home(request):
 	return render(request, 'home.html')
@@ -23,39 +19,14 @@ def portfolio(request):
 	stock = Stock.objects.all().values()
 	objective = {
 		'stock': stock,
-		'amount': 'massive',
 		'update': 'Your objectives lie just ahead, soldier.  No point in turning back now.'
 	}	
 
 	#try:
-	#	details = Stock.objects.get(pk=id)
+	#	stock = Stock.objects.get(pk=id)
 	#except Stock.DoesNotExist:
 	#	raise Http404("Company does not exist")
 	return render(request, 'portfolio.html', objective)	
 
 def logout(request):
 	return render(request, 'logout.html')
-
-def buy(request):
-	return render(request, 'buy.html')
-
-def sell(request):
-	return render(request, 'sell.html')
-	
-"""
-def details(request, id):
-			return render(request, 'details.html', context)	
-"""
-
-
-
-"""
-	count = Portfolio.objects.count()
-
-	context = {
-
-		'count': count,
-
-		'page': 'glad to see you on the front lines',
-	}
-"""
