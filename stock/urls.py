e86b9a5f5fc1914e0499dc4c8a2d0ef4c655af75
jbossgotskillz <160427209+jbossgotskillz@users.noreply.github.com>
2024-07-30 00:56:09 -0500

    Update urls.py

--- stock/urls.py
+++ stock/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-# from django.contrib import admin
 
 from . import views
 
@@ -9,8 +8,4 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('portfolio/', views.portfolio, name='portfolio'),
     path('logout/', views.logout, name='logout'),
-    # path('<int:id>/details', views.details, name='details'),
-    path('buy/', views.buy, name='buy'),            #add <int:id>
-    path('sell/', views.sell, name='sell'),         #add <int:id>
-    # path('goodbye/', views.goodbye, name='goodbye'),
 ]
