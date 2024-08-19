9a8e40a309aa0f05e5d9a4efea8b31315c5448d8
jbossgotskillz <160427209+jbossgotskillz@users.noreply.github.com>
2024-08-19 14:10:16 -0500

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -16,7 +16,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap_themes',
     'django_bootstrap5',
     'stock.apps.StockConfig',
 ]
